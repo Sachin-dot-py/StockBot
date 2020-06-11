1f40a56e5be7f59470af1df1374d81bf6c8c523b
Sachin Ramanathan <sachin.ramanathan123@gmail.com>
2020-06-11 11:19:54 +0800

    Revert back to global path

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def ngrok():
     """ Starts ngrok and returns url """
     url = "https://" + subprocess.check_output(r"""curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'""", shell=True).decode('utf-8').strip('\n')
     if url == 'https://':
-        os.system('/usr/local/bin/ngrok http 5000 > /dev/null &')
+        os.system('ngrok http 5000 > /dev/null &')
         url = "https://" + subprocess.check_output(r"""curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'""", shell=True).decode('utf-8').strip('\n')
     if url == 'https://':
         logging.critical("Failure in obtaining ngrok url")
